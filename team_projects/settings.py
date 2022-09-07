8305b55dd2ec12553291b8b82205ab77c31fca93
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-09-07 21:05:31 +0900

    セキュリティ上の問題の修正

--- team_projects/settings.py
+++ team_projects/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0q7-d5j3lqh7+ls15=g@fv18m4t=-tfxjxn5c%6cmah_*tnr9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
