c379113f85915f41dff0ea38bb0d92a83dcae963
arks22 <sasaki.10222022@gmail.com>
2022-09-11 21:23:51 +0900

    カラムレイアウト

--- users/views.py
+++ users/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import CustomUser
+from diaries.models import Diary
 
 # Create your views here.
 
 def index(request):
     users = CustomUser.objects.all()
-    context = { 'users_list': users,}
-    return render(request, 'users/index.html', context)
+    params = { 'users_list': users,}
+    return render(request, 'users/index.html', params)
 
 def detail(request, user_id):
     user = CustomUser.objects.get(pk=user_id)
-    context = { 'user': user,}
-    return render(request, 'users/detail.html', context)
+    diaries = Diary.objects.filter(student_id=user_id)
+    params = { 
+            'user': user,
+            'diaries_list' : diaries,
+        }
+    return render(request, 'users/detail.html', params)
