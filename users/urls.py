0b830d913c928db8b5d6d8b8bdaf7f3b5dcdbcee
ShinnosukeIto <108009063+shinnosuke0925@users.noreply.github.com>
2022-09-11 23:01:03 +0900

    ログイン後のページ遷移

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('<int:user_id>/',views.detail, name='detail'),
+    path('/',views.detail, name='detail'),
     path('login/', views.MyLoginView.as_view(), name="login"),
     path('logout/', views.MyLogoutView.as_view(), name="logout"),
     path('index/',views.IndexView.as_view(), name="index")
