20ec15178a4a3d5d75f03074b1009e2481534ba6
UCHIDA Yoshihiro <takuyashinta95@gmail.com>
2022-09-11 12:16:47 +0900

    records のURLの修正確認

--- diaries/urls.py
+++ diaries/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'diaries'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:diary_id>/', views.detail, name='detail')
