cebf96e07ac3facc63205488034a13958613b054
ShinnosukeIto <108009063+shinnosuke0925@users.noreply.github.com>
2022-09-11 11:55:12 +0900

    空白問題解決

--- subjects/views.py
+++ subjects/views.py
@@ -5,5 +5,4 @@ from .models import Subject
 def index(request):
  subjects = Subject.objects.all()
  context = { 'subjects_list': subjects,}
- print(context)
- return render(request, 'index.html', context)
+ return render(request, 'subjects/index.html', context)
