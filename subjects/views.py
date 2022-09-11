de5ce42c03627e0daece9315f192abdff586a98c
arks22 <sasaki.10222022@gmail.com>
2022-09-11 11:56:50 +0900

    modify routing

--- subjects/views.py
+++ subjects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Subject
 
 def index(request):
- subjects = Subject.objects.all()
- context = { 'subjects_list': subjects,}
- print(context)
- return render(request, 'index.html', context)
+    subjects = Subject.objects.all()
+    context = { 'subjects_list': subjects,}
+    print(context)
+    return render(request, 'subjects/index.html', context)
