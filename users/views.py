449d3d5a2dd26cac98bb7a0e948b80fd3316a7af
ShinnosukeIto <108009063+shinnosuke0925@users.noreply.github.com>
2022-09-11 21:17:29 +0900

    ログイン機能の移植

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import CustomUser
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from . import forms
 
 # Create your views here.
 
@@ -13,3 +17,13 @@ def detail(request, user_id):
     user = CustomUser.objects.get(pk=user_id)
     context = { 'user': user,}
     return render(request, 'users/detail.html', context)
+
+class MyLoginView(LoginView):
+    form_class = forms.LoginForm
+    template_name = "users/login.html"
+
+class MyLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = "users/logout.html"
+
+class IndexView(TemplateView):
+    template_name = "users/index.html"
