6a28badfa1169eb957a38fdc09d5aaafdcb4c3ab
ShinnosukeIto <108009063+shinnosuke0925@users.noreply.github.com>
2022-09-13 23:06:32 +0900

    コミットしたいだけ2

--- users/views.py
+++ users/views.py
@@ -22,14 +22,11 @@ def index(request):
     params = {'users_list': users,}
     return render(request, 'users/index.html', params)
 
-<<<<<<< HEAD
-=======
 def teacher_index(request):
     users = CustomUser.objects.filter(job="teacher")
     params = { 'users_list': users,}
     return render(request, 'users/teacher_index.html', params)
 
->>>>>>> b7a9b9c0e441704d27e315c912bebd4936ddde3b
 def detail(request, user_id):
     user = CustomUser.objects.get(pk=user_id)
     diaries = Diary.objects.filter(student = user)
@@ -43,46 +40,6 @@ def detail(request, user_id):
     }
 
     return render(request, 'users/detail.html', params)
-
-<<<<<<< HEAD
-def ConvertToGrade(today, birthday):
-    a = "0401"
-    birthmd = birthday.strftime("%Y%m%d")[4:]
-    birthy= birthday.strftime("%Y%m%d")[0:4]
-    todayy=today.strftime("%Y%m%d")[0:4]
-    if int(birthmd) < int(a) :#早生まれ
-        g=int(todayy)-int(birthy)
-    else:#遅生まれ
-        g=int(todayy)-int(birthy)-1
-    grade=""
-    if g==6:
-        grade="小学1年生"
-    elif g==7:
-        grade="小学2年生"
-    elif g==8:
-        grade="小学3年生"
-    elif g==9:
-        grade="小学4年生"
-    elif g==10:
-        grade="小学5年生"
-    elif g==11:
-        grade="小学6年生"
-    elif g==12:
-        grade="中学1年生"
-    elif g==13:
-        grade="中学2年生"
-    elif g==14:
-        grade="中学3年生"
-    elif g==15:
-        grade="高校1年生"
-    elif g==16:
-        grade="高校2年生"
-    elif g==17:
-        grade="高校3年生"
-
-    return grade
-
-=======
 def follow(request, user_id):
     following = CustomUser.objects.get(pk=user_id)
     Follow.objects.get_or_create(owner=request.user, follow_target=following)
@@ -140,8 +97,6 @@ def edit(request,user_id):
         'form':forms.CustomUserCreationForm(instance=obj)
     }
     return render(request,'users/edit.html',params)
-
->>>>>>> b7a9b9c0e441704d27e315c912bebd4936ddde3b
 
 class MyLoginView(LoginView):
     form_class = forms.LoginForm
