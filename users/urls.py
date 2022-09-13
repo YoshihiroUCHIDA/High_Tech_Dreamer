8fd158d517c45747b0bdca1ca7a7fb0db3b604a8
arks22 <sasaki.10222022@gmail.com>
2022-09-13 19:12:19 +0900

    ルーティング設定

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,10 @@ app_name = 'users'
 urlpatterns = [
     path('<int:user_id>/',views.detail, name='detail'),
     path('<int:user_id>/records/',records_views.index, name='records_detail'),
+    path('<int:user_id>/follow/',views.follow, name="follow"),
     path('login/', views.MyLoginView.as_view(), name="login"),
     path('logout/', views.MyLogoutView.as_view(), name="logout"),
     path('index/',views.index, name="index"),
-    path('edit/<int:user_id>/',views.edit,name="edit"),
+    path('<int:user_id>/edit/',views.edit,name="edit"),
     path('teacher_index/',views.teacher_index, name="teacher_index"),
-    path('follow/<int:user_id>/',views.follow, name="follow"),
 ]
